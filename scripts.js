cb66125c8b286f900487662406c5c689a8fafd92
Heather Essex <heatherressex@gmail.com>
2020-10-19 17:08:30 +0100

    Animated burger button integrated

--- scripts.js
+++ scripts.js
@@ -1,13 +1,8 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    $( ".burgericon" ).click(function() {
-      $( ".burgermenu" ).toggleClass("active");
-      $( ".burgerclose" ).toggleClass("active");
-      $( ".burgeropen" ).toggleClass("inactive");
-    });
-
     $( ".hamburger" ).click(function() {
+      $( ".burgermenu" ).toggleClass("active");
       $( ".hamburger" ).toggleClass("is-active");
     });
 });
