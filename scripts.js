215046789a07b135a65dd967ae4942f1942a6a08
Heather Essex <heatherressex@gmail.com>
2020-10-19 16:45:25 +0100

    Practice with animated menu button

--- scripts.js
+++ scripts.js
@@ -2,8 +2,12 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
     $( ".burgericon" ).click(function() {
-        $( ".burgermenu" ).toggleClass("active");
-        $( ".burgerclose" ).toggleClass("active");
-        $( ".burgeropen" ).toggleClass("inactive");
-      });
+      $( ".burgermenu" ).toggleClass("active");
+      $( ".burgerclose" ).toggleClass("active");
+      $( ".burgeropen" ).toggleClass("inactive");
+    });
+
+    $( ".hamburger" ).click(function() {
+      $( ".hamburger" ).toggleClass("is-active");
+    });
 });
